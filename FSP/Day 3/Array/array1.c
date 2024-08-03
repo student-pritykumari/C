612b1e07b8fe9b33e3abd3d865bd1a76c884c6dc
student-pritykumari <prity.r.2004@gmail.com>
2024-08-03 13:42:39 +0530

    day 3 files

---
+++ FSP/Day 3/Array/array1.c
@@ -0,0 +1,30 @@
+/*
+Array Fill up:
+	
+	  0   1   2   3   4
+--------------------------
+ 0 |  1   2   3   4   5 => 0 + (col+1)
+ 1 |  6   7   8   9  10 =>5 + (col+1)
+ 2 | 11  12  13  14  15 => 10 + (col+1)
+ 3 | 16  17  18  19  20=> 15 + (col+1)
+ 4 | 21  22  23  24  25 => 20 + (col+1)
+--------------------------
+*/
+#include <stdio.h>
+int main(void) {
+	int arr[5][5], row, col, maxloc = 5;
+	printf("\nInitializing the content of the array...");
+	for (row=0; row<maxloc; row++) {
+		for (col=0; col<maxloc; col++) {
+			arr[row][col] = row * maxloc +(col + 1);
+			}
+	}
+	printf("\n\nDisplaying the content of the array...");
+	for (row=0; row<maxloc; row++) {
+		for (col=0; col<maxloc; col++) {
+			printf("%5d", arr[row][col]);
+		}
+		printf("\n");
+	}
+	printf("\n\nEnd of the program...");
+}
---
+++ FSP/Day 3/Array/array3.c
@@ -0,0 +1,34 @@
+/*
+Array Fill up:
+maxloc = 5
+	        col
+	   0   1   2   3   4
+--------------------------
+r 0 |  10  0   0   0  40  
+o 1 |  0  10   0  40   0  
+w 2 |  0   0  50   0   0  
+  3 |  0  40   0  10   0  
+  4 | 40   0   0   0  10  
+--------------------------
+*/
+#include <stdio.h>
+
+int arr[5][5];
+int main(void) {
+	int row, col, maxloc = 5;
+	printf("\nInitializing the content of the array...");
+	for (row=0; row<maxloc; row++) {
+		for (col=0; col<maxloc; col++) {
+			if (row == col) arr[row][col] = 10;
+			if (row + col == maxloc - 1)arr[row][col] += 40;
+		}
+	}
+	printf("\n\nDisplaying the content of the array...\n");
+	for (row=0; row<maxloc; row++) {
+		for (col=0; col<maxloc; col++) {
+			printf("%5d", arr[row][col]);
+		}
+		printf("\n");
+	}
+	printf("\n\nEnd of the program...");
+}
